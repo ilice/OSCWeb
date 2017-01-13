4f89f75c3d436a4c3d5eafc03bec2e5e2446d764
teanocrata <teanocrata@gmail.com>
2017-01-13 15:15:41 +0100

    Omnibox in map (not functional)

--- src/maps/index.js
+++ src/maps/index.js
@@ -4,8 +4,10 @@ var page = require('page')
 var header = require('../header')
 var footer = require('../footer')
 var map = require('../map')
+var omnibox = require('../omnibox')
 
 page('/', header, footer, function (ctx, next) {
   var main = document.getElementById('main-container')
   map.addMap(main)
+  main.appendChild(omnibox)
 })
---
+++ src/omnibox/index.js
@@ -0,0 +1,5 @@
+var yo = require('yo-yo')
+
+module.exports = yo`<div class='omnibox'>
+  <i class="material-icons">menu</i> <input type="text" placeholder="Buscar en Open Smart Country"/> <i class="material-icons flex-end">search</i>
+</div>`
