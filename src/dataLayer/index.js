6f09dfd36678666f669f8238a7abf5bc2271e6b3
teanocrata <teanocrata@gmail.com>
2017-01-25 15:05:49 +0100

    Fixed some errors in test

--- src/dataLayer/index.js
+++ src/dataLayer/index.js
@@ -2,22 +2,23 @@ var request = require('superagent')
 
 function createLayerFromBoundingBox (bbox, next) {
   var layer
-  checkBbox(bbox, next)
   loadLayer(layer, bbox, next)
 }
 
-function checkBbox (bbox, next) {
-  if (!bbox) {
-    next(new ReferenceError('Bounding box cannot be empty'))
-  }
-  if (bbox.length !== 4) next(new Error('Bounding box must be a 4 array'))
-  bbox.forEach(function (element) {
-    if (typeof element !== 'number') next(Error('Bounding box elements must be a number'))
-  })
+function checkBbox (bbox) {
+  if (!bbox) return new ReferenceError('Bounding box cannot be empty')
+
+  if (bbox.length !== 4) return new Error('Bounding box must be a 4 array')
+
+  var err
+  bbox.forEach(item => { if (typeof item !== 'number') err = new Error('Bounding box elements must be a number') })
+  return err
 }
 
 function loadLayer (layer, bbox, next) {
-  request
+  var err = checkBbox(bbox)
+  if (!err) {
+    request
   .get('https://opensmartcountry.com/cadastral/parcel')
   .query({ bbox: bbox.toString() })
   .end(function (err, res) {
@@ -25,6 +26,9 @@ function loadLayer (layer, bbox, next) {
     layer = res.body
     next(err, layer)
   })
+  } else {
+    next(err)
+  }
 }
 
 module.exports = {
--- test/dataLayer.test.js
+++ test/dataLayer.test.js
@@ -60,7 +60,7 @@ describe('Data Layer', function () {
     it('should return error if bounding Box parameter length is not four', function (done) {
       var dataLayer = require('../src/dataLayer')
       var bbox = [40.437603, -5.762986, 40.439442]
-      dataLayer.createLayerFromBoundingBox(bbox, function (err, layer) {
+      dataLayer.createLayerFromBoundingBox(bbox, function (err) {
         expect(err).to.be.an('error')
         expect(err).to.be.an.instanceof(Error)
         expect(err.message).to.be.equal('Bounding box must be a 4 array')
@@ -71,7 +71,7 @@ describe('Data Layer', function () {
     it('should return error if Bounding Box parameter elements are not numbers', function (done) {
       var dataLayer = require('../src/dataLayer')
       var bbox = [40.437603, -5.762986, '40.439442', -5.757841]
-      dataLayer.createLayerFromBoundingBox(bbox, function (err, layer) {
+      dataLayer.createLayerFromBoundingBox(bbox, function (err) {
         expect(err).to.be.an('error')
         expect(err).to.be.an.instanceof(Error)
         expect(err.message).to.be.equal('Bounding box elements must be a number')
@@ -79,25 +79,26 @@ describe('Data Layer', function () {
       })
     })
 
-    it('should obtain poligon coordinates for Feature cadastral parcel in bounding box', function () {
+    it('should obtain poligon coordinates for Feature cadastral parcel in bounding box', function (done) {
       var dataLayer = require('../src/dataLayer')
       var bbox = [40.43917746743397, -5.757829017646071, 40.439409063444636, -5.757541644814119]
-      dataLayer = dataLayer.createLayerFromBoundingBox(bbox, function (err, layer) {
+      dataLayer.createLayerFromBoundingBox(bbox, function (err, layer) {
         expect(err).to.be.a('null')
         expect(layer).to.have.property('type', 'FeatureCollection')
         expect(layer).to.have.property('features')
-        expect(layer.features.length).to.be.at.least(1, 'should obtain at least 1 cadastral parcel')
+        expect(layer.features.length).to.be.at.least(1, 'should obtain at least 2 cadastral parcel')
         expect(layer.features[0]).to.have.property('type', 'Feature')
         expect(layer.features[0]).to.have.property('geometry')
         expect(layer.features[0].geometry).to.have.property('type', 'polygon')
         expect(layer.features[0].geometry).to.have.property('coordinates')
         expect(layer.features[0].geometry.coordinates.length).to.be.at.least(1, 'should obtain at least 1 LinearRing coordinate array')
         expect(layer.features[0].geometry.coordinates[0].length).to.be.at.least(4, 'LinearRing is closed LineString with 4 or more positions')
-        expect(layer.features[0].geometry.coordinates[0][0]).to.eql(dataLayer.features[0].geometry.coordinates[0][dataLayer.features[0].geometry.coordinates[0].length - 1])
+        expect(layer.features[0].geometry.coordinates[0][0]).to.eql(layer.features[0].geometry.coordinates[0][layer.features[0].geometry.coordinates[0].length - 1])
+        done()
       })
     })
 
-    it('should obtain bbox envelope for Feature cadastral parcel in bounding box', function () {
+    it('should obtain bbox envelope for Feature cadastral parcel in bounding box', function (done) {
       var dataLayer = require('../src/dataLayer')
       var bbox = [40.43917746743397, -5.757829017646071, 40.439409063444636, -5.757541644814119]
       dataLayer.createLayerFromBoundingBox(bbox, function (err, layer) {
@@ -110,10 +111,11 @@ describe('Data Layer', function () {
         expect(layer.features[0].bbox.coordinates[0][1]).to.be.a('number')
         expect(layer.features[0].bbox.coordinates[1][0]).to.be.a('number')
         expect(layer.features[0].bbox.coordinates[1][1]).to.be.a('number')
+        done()
       })
     })
 
-    it('should obtain Feature cadastral parcel properties in bounding box', function () {
+    it('should obtain Feature cadastral parcel properties in bounding box', function (done) {
       var dataLayer = require('../src/dataLayer')
       var bbox = [40.43917746743397, -5.757829017646071, 40.439409063444636, -5.757541644814119]
       dataLayer.createLayerFromBoundingBox(bbox, function (err, layer) {
@@ -127,6 +129,7 @@ describe('Data Layer', function () {
         expect(layer.features[0].properties).to.have.property('endLifespanVersion')
         expect(layer.features[0].properties).to.have.property('areaValue')
         expect(layer.features[0].properties).to.have.property('cadastralData')
+        done()
       })
     })
   })
