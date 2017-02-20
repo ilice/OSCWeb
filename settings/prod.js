363e09cbebe37074eaffa3312ea724d3bf87994f
teanocrata <teanocrata@gmail.com>
2017-02-20 10:57:14 +0100

    Temp production config

--- settings/prod.js
+++ settings/prod.js
@@ -1,5 +1,5 @@
 const secrets = require('./secrets')
 module.exports = {
   GOOGLE_API_KEY: secrets.GOOGLE_API_KEY,
-  CADASTRAL_PARCEL_URI: 'http://dev.opensmartcountry.com:80/api/cadastral/parcel/'
+  CADASTRAL_PARCEL_URI: '/api/cadastral/parcel/'
 }
